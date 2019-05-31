17a238444aea1e26dcc20b3607f45a2433705d01
Халилов Рустам - LN <konversys@gmail.com>
2019-05-31 16:44:40 +0700

    Исправлено отображение времени таймера

--- app/src/main/java/com/example/myapplication/model/logic/Tools.java
+++ app/src/main/java/com/example/myapplication/model/logic/Tools.java
@@ -1,8 +1,6 @@
 package com.example.myapplication.model.logic;
 
 import org.joda.time.Duration;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,11 +19,11 @@ public class Tools {
         return new SimpleDateFormat("yyyy-MM-dd").format(date);
     }
 
-    public static Date getFormattedDateWithoutTime(Long date){
+    public static Date getFormattedDateWithoutTime(Long date) {
         return new Date(date);
     }
 
-    public static String getFormattedStringDateDots(Date date){
+    public static String getFormattedStringDateDots(Date date) {
         return new SimpleDateFormat("dd.MM.yyyy").format(date);
     }
 
@@ -33,20 +31,28 @@ public class Tools {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date(dateTime));
     }
 
-    public static LocalTime getDifferenceOfStationDates(Date date, Date subject){
-        Duration duration = new Duration(date.getTime(), subject.getTime());
-        LocalTime result = new LocalTime(duration.getMillis());
-        return result;
+    public static Duration getDifferenceOfStationDates(Date date, Date subject) {
+        if (date == null || subject == null)
+            return null;
+        return new Duration(date.getTime(), subject.getTime());
     }
 
-    public static String getStringDifferenceOfStationDates(LocalTime time){
-        String result = "";
-        if (time.getHourOfDay() != 0)
-            result += String.format("%02dч. ",time.getHourOfDay());
-        if (time.getHourOfDay() != 0 && time.getMinuteOfHour() != 0)
-            result += String.format("%02dм. ",time.getMinuteOfHour());
-        if (time.getHourOfDay() != 0 && time.getMinuteOfHour() != 0 && time.getSecondOfMinute() != 0)
-            result += String.format("%02dс. ",time.getSecondOfMinute());
-        return result;
+    public static String getStringDifferenceOfStationDates(Duration time) {
+        if (time == null)
+            return "Неизвестно";
+        StringBuilder result = new StringBuilder();
+        long sec = time.getStandardSeconds();
+        long days = sec / 60 / 60 / 24;
+        long hours = sec / 60 / 60 - days * 24;
+        long minutes = sec / 60 - days * 24 * 60 - hours * 60;
+        long seconds = sec - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+
+        if (days != 0)
+            result.append(String.format("%02dд. ", days));
+        if (days != 0 || hours != 0)
+            result.append(String.format("%02dч. ", hours));
+        result.append(String.format("%02dм. ", minutes));
+        result.append(String.format("%02dс. ", seconds));
+        return result.toString();
     }
 }
